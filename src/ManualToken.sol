b8396fe65777b1d2255840353d4d6ad5be4a1a14
David Parseen <david.parseen@gmail.com>
2025-02-06 19:18:17 +0300

    feat: implement ManualToken contract with basic ERC20 functionality

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -1,3 +1,35 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.19;
+
+// Token standard reference: https://eips.ethereum.org/EIPS/eip-20
+// ERC20 Requirements: https://eips.ethereum.org/EIPS/eip-20#specification
+
+contract ManualToken {
+    mapping (address => uint256) private balances;
+
+    function name() public pure returns (string memory) {
+        return "Manual Token";
+    }
+
+    function totalSupply() public pure returns (uint256) {
+        return 100 ether;
+    }
+
+    function decimals() public pure returns (uint8) {
+        return 18;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256) {
+        return balances[_owner];
+    }
+
+    function  transfer(address _to, uint256 _value) public {
+        uint256 previousBalances = balanceOf(msg.sender) + balanceOf(_to);
+        balances[msg.sender] -= _value;
+        balances[_to]  += _value;
+
+        require(balanceOf(msg.sender) + balanceOf(_to) == previousBalances);
+
+    }
+}
