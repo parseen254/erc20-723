51e0962921964e2bf6e21f63b75198193ad2e403
David Parseen <david.parseen@gmail.com>
2025-02-06 20:14:57 +0300

    feat: enhance deployment script to return TokenFromOpenZeppelin instance and add tests for token functionality

--- script/DeployTOZ.s.sol
+++ script/DeployTOZ.s.sol
@@ -7,9 +7,10 @@ import { TokenFromOpenZeppelin } from "../src/TokenFromOpenZeppelin.sol";
 contract DeployTOZ is Script {
     uint256 public constant INITIAL_SUPPLY = 1000 ether;
 
-    function run() external {
+    function run() external returns (TokenFromOpenZeppelin){
         vm.startBroadcast();
-        new TokenFromOpenZeppelin(INITIAL_SUPPLY);
+        TokenFromOpenZeppelin token = new TokenFromOpenZeppelin(INITIAL_SUPPLY);
         vm.stopBroadcast();
+        return token;
     }
 }
---
+++ test/TestTOZ.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import { Test } from "forge-std/Test.sol";
+import { DeployTOZ } from "../script/DeployTOZ.s.sol";
+import { TokenFromOpenZeppelin } from "../src/TokenFromOpenZeppelin.sol";
+
+contract TestTOZ is Test {
+    TokenFromOpenZeppelin public toz;
+    DeployTOZ public deployer;
+
+    address bob = makeAddr("bob");
+    address alice = makeAddr("alice");
+
+    uint256 public constant STARTING_BALANCE = 100 ether;
+
+    function setUp() public {
+        deployer = new DeployTOZ();
+        toz = deployer.run();
+
+        vm.prank(msg.sender);
+        toz.transfer(bob, STARTING_BALANCE);
+    }
+
+    function testBobBalance() public view {
+        assertEq(toz.balanceOf(bob), STARTING_BALANCE);
+    }
+
+    function testAliceBalance() public view {
+        assertEq(toz.balanceOf(alice), 0);
+    }
+
+    function testAllowance() public {
+        uint256 initialAllowance = 1000;
+        vm.prank(bob);
+        toz.approve(alice, initialAllowance);
+        assertEq(toz.allowance(bob, alice), initialAllowance);
+
+        vm.prank(alice);
+        uint256 transferAmount = 100;
+        toz.transferFrom(bob, alice, transferAmount);
+        assertEq(toz.balanceOf(bob), STARTING_BALANCE - transferAmount);
+        assertEq(toz.balanceOf(alice), transferAmount);
+    }
+}
